1f7a946ca2b3f6a86b10f401353e4975e63b27f0
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2020-09-17 10:39:30 -0500

    added a route to get reports by the coresponding category

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get('/commodities', async (req, res, next) => {
   res.json(json);
 });
 
+app.get('/commodities/:id', async (req, res, next) => {
+  const comId = req.params.id;
+  const resp = await fetch(`https://mymarketnews.ams.usda.gov/public_data/ajax-get-commodities/${comId}`, {
+    rejectUnauthorized: false
+  });
+  const json = await resp.json()
+  res.json(json);
+});
+
 app.get('/offices', async (req, res, next) => {
   const resp = await fetch('https://mymarketnews.ams.usda.gov/public_data/ajax-get-offices/', {
     rejectUnauthorized: false
@@ -19,8 +28,26 @@ app.get('/offices', async (req, res, next) => {
   res.json(json);
 });
 
+app.get('/offices/:id', async (req, res, next) => {
+  const comId = req.params.id;
+  const resp = await fetch(`https://mymarketnews.ams.usda.gov/public_data/ajax-get-offices/${comId}`, {
+    rejectUnauthorized: false
+  });
+  const json = await resp.json()
+  res.json(json);
+});
+
 app.get('/market-types', async (req, res, next) => {
   const resp = await fetch('https://mymarketnews.ams.usda.gov/public_data/ajax-get-market-types/', {
+    rejectUnauthorized: false
+  });
+  const json = await resp.json()
+  res.json(json);
+});
+
+app.get('/market-types/:id', async (req, res, next) => {
+  const comId = req.params.id;
+  const resp = await fetch(`https://mymarketnews.ams.usda.gov/public_data/ajax-get-market-types/${comId}`, {
     rejectUnauthorized: false
   });
   const json = await resp.json()

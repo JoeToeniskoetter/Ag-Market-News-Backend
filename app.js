2547c45631548961b019f92d59c9c3404d421dab
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2020-10-03 17:30:12 -0500

    added new route to proxy pdf requests

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
-const app = require('express')();
-const fetch = require('node-fetch');
-const request = require('request')
+const app = require("express")();
+const fetch = require("node-fetch");
+const request = require("request");
 const rateLimit = require("express-rate-limit");
-require('dotenv').config()
+const e = require("express");
+require("dotenv").config();
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 const limiter = rateLimit({
   windowMs: 10 * 1000, // 10 seconds
-  max: 100 // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 
 //  apply to all requests
 app.use(limiter);
 app.use(errorHandler);
 
-app.get('/report/:id', async (req, res) => {
+app.get("/report/:id", async (req, res, next) => {
   const repId = req.params.id;
   const pdfuri = `https://www.ams.usda.gov/mnreports/${repId}.pdf`;
-  const txturi = `https://www.ams.usda.gov/mnreports/${repId}.txt`;
-  try {
-    const resp = await fetch(pdfuri);
-    console.log(resp.status)
-    if (resp.status != 200) {
-      return res.send(txturi)
-    }
-    return res.send(pdfuri);
-  } catch (e) {
-    console.log(e)
-    res.send(txturi)
+  const resp = await fetch(pdfuri);
+  if (resp.status === 200) {
+    request(pdfuri).pipe(res);
+  } else {
+    return next();
   }
-
 });
 
-app.get('/commodities', async (req, res, next) => {
+app.get("/commodities", async (req, res, next) => {
   try {
-    const url = 'https://mymarketnews.ams.usda.gov/public_data/ajax-get-commodities/';
+    const url =
+      "https://mymarketnews.ams.usda.gov/public_data/ajax-get-commodities/";
     const resp = await fetch(url);
     const json = await resp.json();
     res.json(json);
   } catch (e) {
-    return next(e)
+    return next(e);
   }
 });
 
-app.get('/commodities/:id', async (req, res, next) => {
+app.get("/commodities/:id", async (req, res, next) => {
   try {
     const comId = req.params.id;
     const url = `https://mymarketnews.ams.usda.gov/public_data/ajax-get-commodities/${comId}`;
@@ -54,22 +49,23 @@ app.get('/commodities/:id', async (req, res, next) => {
     const json = await resp.json();
     res.json(json);
   } catch (e) {
-    return next(e)
+    return next(e);
   }
 });
 
-app.get('/offices', async (req, res, next) => {
+app.get("/offices", async (req, res, next) => {
   try {
-    const url = 'https://mymarketnews.ams.usda.gov/public_data/ajax-get-offices/';
+    const url =
+      "https://mymarketnews.ams.usda.gov/public_data/ajax-get-offices/";
     const resp = await fetch(url);
     const json = await resp.json();
     res.json(json);
   } catch (e) {
-    next(e)
+    next(e);
   }
 });
 
-app.get('/offices/:id', async (req, res, next) => {
+app.get("/offices/:id", async (req, res, next) => {
   try {
     const comId = req.params.id;
     const url = `https://mymarketnews.ams.usda.gov/public_data/ajax-get-offices/${comId}`;
@@ -77,22 +73,23 @@ app.get('/offices/:id', async (req, res, next) => {
     const json = await resp.json();
     res.json(json);
   } catch (e) {
-    next(e)
+    next(e);
   }
 });
 
-app.get('/market-types', async (req, res, next) => {
+app.get("/market-types", async (req, res, next) => {
   try {
-    const url = 'https://mymarketnews.ams.usda.gov/public_data/ajax-get-market-types/';
+    const url =
+      "https://mymarketnews.ams.usda.gov/public_data/ajax-get-market-types/";
     const resp = await fetch(url);
     const json = await resp.json();
     res.json(json);
   } catch (e) {
-    return next(e)
+    return next(e);
   }
 });
 
-app.get('/market-types/:id', async (req, res, next) => {
+app.get("/market-types/:id", async (req, res, next) => {
   try {
     const comId = req.params.id;
     const url = `https://mymarketnews.ams.usda.gov/public_data/ajax-get-market-types/${comId}`;
@@ -100,42 +97,43 @@ app.get('/market-types/:id', async (req, res, next) => {
     const json = await resp.json();
     res.json(json);
   } catch (e) {
-    return next(e)
+    return next(e);
   }
 });
 
-app.get('/reports', async (req, res) => {
-
+app.get("/reports", async (req, res) => {
   try {
-    const resp = await fetch('https://marsapi.ams.usda.gov/services/v1.1/reports', {
-      headers: {
-        Authorization: `Basic ${process.env.API_KEY}`
+    const resp = await fetch(
+      "https://marsapi.ams.usda.gov/services/v1.1/reports",
+      {
+        headers: {
+          Authorization: `Basic ${process.env.API_KEY}`,
+        },
       }
-    });
+    );
     const json = await resp.json();
-    const reduced = json.map(x => ({
+    const reduced = json.map((x) => ({
       slug_name: x.slug_name,
-      report_title: x.report_title
+      report_title: x.report_title,
     }));
     res.json(reduced);
   } catch (e) {
-    res.send(e.message)
+    res.send(e.message);
   }
 });
 
-app.get('*', (req, res, next) => {
+app.get("*", (req, res, next) => {
   res.status(404).json({
-    message: 'Not Found'
-  })
-})
+    message: "Not Found",
+  });
+});
 
 function errorHandler(err, req, res, next) {
   if (res.headersSent) {
-    return next(err)
+    return next(err);
   }
-  res.status(500)
-  res.render('error', { error: err.message })
+  res.status(500);
+  res.render("error", { error: err.message });
 }
-
 
 module.exports = app;

87218de6832093af827cb636425b979219d58f28
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2021-02-05 08:31:11 -0600

    added function to track requests

--- src/cache/cache.ts
+++ src/cache/cache.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { Requests } from "../db/db";
 
 type cacheType = {
   initDate: number;
@@ -13,7 +14,25 @@ export function cacheResult(path: string, data: Object) {
   cache[path] = JSON.stringify(data);
 }
 
+export async function incrementRequest() {
+  const now = new Date().toDateString();
+  const foundRequestDate = await Requests.findOne({ date: now });
+
+  if (!foundRequestDate) {
+    console.log("didnt find req");
+    const newR = new Requests();
+    newR.date = now;
+    newR.numberOfRequests = 1;
+    return await newR.save();
+  } else {
+    console.log("found req");
+    foundRequestDate.numberOfRequests++;
+    await foundRequestDate.save();
+  }
+}
+
 export function Cache(req: Request, res: Response, next: NextFunction) {
+  incrementRequest();
   const ONE_HOUR = 60 * 60 * 1000;
   const now = Date.now();
   const expired = now - cache.initDate > ONE_HOUR;
